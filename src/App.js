65a4963d665856fc3ea150e6635c70ae5eb1e957
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-02-25 16:36:23 -0300

    Navbar, footer about e home pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+//pages
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-     <h1>Miniblog</h1>
+     <BrowserRouter>
+     <Navbar/>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+        </Routes> 
+      </div>
+      <Footer/>
+     </BrowserRouter>
     </div>
   );
 }
