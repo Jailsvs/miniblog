4e2af57f8c80791a67730fcdf08334459322d5cf
Jailson Volnei dos Santos <jailsvs@gmail.com>
2023-02-25 16:09:29 -0300

    Start do projeto com firebase

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQOufR57xfG6vHqUWul9HrI8xdM3MSjsM",
+  authDomain: "miniblog-443d2.firebaseapp.com",
+  projectId: "miniblog-443d2",
+  storageBucket: "miniblog-443d2.appspot.com",
+  messagingSenderId: "623555390802",
+  appId: "1:623555390802:web:835a42358200fc11b49e8b"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export {db};
